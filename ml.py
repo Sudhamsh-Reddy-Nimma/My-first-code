56278127f78fd601a8ade8f0da671a9fd054ffb2
Sudhamsh Reddy Nimma <78696933+Sudhamsh-Reddy-Nimma@users.noreply.github.com>
2021-02-13 20:57:35 +0530

    Add files via upload

---
+++ ml.py
@@ -0,0 +1,24 @@
+print("       #         #        #       #")
+print("      # #       # #      # #     # #")
+print("     #   #     #   #    #   #   #   #")
+print("                            ")
+for i in range(2):
+    print("############################################")
+print("                            ")
+for s in range(1):
+    print("     ####       ####     ####    ####   ")
+
+print("                                      ")
+for z in range(2):
+    print("#############################################")
+edit = input("What do you want to see?: ")
+if edit == "Effiel tower":
+    print("        #        ")
+    print("        #        ")
+    print("        #        ")
+    print("       # #       ")
+    print("      #   #      ")
+    print("     #######     ")
+    print("    #       #    ")
+    print("   ###########   ")
+    print("  #           #  ")
